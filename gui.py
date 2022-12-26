8905fe3792ba96a421d79d1c9f1fb635cd640ead
Kruesman <Kruesman99@gmail.com>
2022-12-26 09:57:44 -0500

    Implemented Edit Button

--- gui.py
+++ gui.py
@@ -4,9 +4,17 @@ import PySimpleGUI as Sg
 label = Sg.Text("Type in a to-do")
 input_box = Sg.InputText(tooltip="Enter todo", key="todo")
 add_button = Sg.Button("Add")
+list_box = Sg.Listbox(values=functions.get_todos(),
+                      key="todos",
+                      enable_events=True,
+                      size=(45, 10))
+edit_button = Sg.Button("Edit")
+
 
 window = Sg.Window('My Todo App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button]],
                    font=("Helvetica", 20))
 
 while True:
@@ -17,6 +25,22 @@ while True:
             new_todo = values["todo"]
             todos.append(new_todo + "\n")
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + '\n'
+
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
         case Sg.WIN_CLOSED:
             break
 
